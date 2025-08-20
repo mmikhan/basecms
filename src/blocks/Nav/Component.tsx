557f3128daa65fe176b789281a3117adc1513c63
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-08-21 01:40:05 +0200

    feat: cleanup

--- src/blocks/Nav/Component.tsx
+++ src/blocks/Nav/Component.tsx
@@ -1,25 +1,17 @@
 import { CMSLink } from '@/components/Link'
-// import { getCachedGlobal } from '@/lib/getGlobals'
-// import type { Header } from '@/payload-types'
 import { SearchIcon } from 'lucide-react'
 import Link from 'next/link'
 import { NavBlock as NavBlockProps } from '@/payload-types'
 import { Media } from '@/components/Media'
 
 export const NavBlock: React.FC<NavBlockProps> = async ({ media, links }) => {
-  //   const { layout } = (await getCachedGlobal('header', 1)()) as Header
-
   return (
     <header className="container mx-auto relative z-20 ">
       <div className="py-8 flex justify-between">
         <Link href="/">
-          {/* <Logo className="invert dark:invert-0" /> */}
           <Media priority resource={media} className="size-10" />
         </Link>
         <nav className="flex gap-3 items-center">
-          {/* {layout.flatMap(({ links }) =>
-            links?.map(({ link }, i) => <CMSLink key={i} {...link} />),
-          )} */}
           {links?.map(({ link }, i) => (
             <CMSLink key={i} {...link} />
           ))}
