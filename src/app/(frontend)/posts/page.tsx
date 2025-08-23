3214d4e03484e0eb84b7d7a733e91109ec0f012f
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-08-23 12:10:19 +0200

    feat: pagination init

--- src/app/(frontend)/posts/page.tsx
+++ src/app/(frontend)/posts/page.tsx
@@ -2,6 +2,7 @@ import { unstable_cache } from 'next/cache'
 import { getPayload } from 'payload'
 import configPromise from '@payload-config'
 import { PageRange } from '@/components/PageRange'
+import { PaginationComponent } from '@/components/Pagination'
 
 export const dynamic = 'force-static'
 
@@ -9,18 +10,28 @@ export default async function PostsArchivePage() {
   const { posts, labels } = await getCachedPosts()
 
   return (
-    <div>
-      {posts.map((post) => (
+    <article>
+      {posts.docs.map((post) => (
         <div key={post.id}>
           <code>{post.id}</code>
           <h2>Title: {post.title}</h2>
           <p>Slug: {post.slug}</p>
         </div>
       ))}
-    </div>
+      <div className="container mb-16">
+        <div className="prose dark:prose-invert max-w-none">
+          <h1>Posts</h1>
+        </div>
+      </div>
+
       <div className="container mb-8">
         <PageRange {...posts} {...labels} limit={12} />
       </div>
+
+      <div className="container">
+        <PaginationComponent {...posts} />
+      </div>
+    </article>
   )
 }
 
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,75 @@
+import type { PaginatedDocs } from 'payload'
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from '@/components/ui/pagination'
+import { cn } from '@/lib/utils'
+
+export const PaginationComponent: React.FC<PaginatedDocs & { className?: string }> = ({
+  className,
+  page = 1,
+  totalPages,
+}) => {
+  const hasNextPage = page < totalPages
+  const hasPrevPage = page > 1
+
+  const hasExtraPrevPages = page - 1 > 1
+  const hasExtraNextPages = page + 1 < totalPages
+
+  return (
+    <div className={cn('my-12', className)}>
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            <PaginationPrevious
+              aria-disabled={!hasPrevPage}
+              className={cn({
+                'pointer-events-none text-muted-foreground': !hasPrevPage,
+              })}
+              href={`/posts/page/${page - 1}`}
+            />
+          </PaginationItem>
+
+          {hasExtraPrevPages && (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          )}
+
+          {hasPrevPage && (
+            <PaginationItem>
+              <PaginationLink href={`/posts/page/${page - 1}`}>{page - 1}</PaginationLink>
+            </PaginationItem>
+          )}
+
+          <PaginationItem>
+            <PaginationLink isActive href={`/posts/page/${page}`}>
+              {page}
+            </PaginationLink>
+          </PaginationItem>
+
+          {hasNextPage && (
+            <PaginationItem>
+              <PaginationLink href={`/posts/page/${page + 1}`}>{page + 1}</PaginationLink>
+            </PaginationItem>
+          )}
+
+          {hasExtraNextPages && (
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+          )}
+
+          <PaginationItem>
+            <PaginationNext aria-disabled={!hasNextPage} href={`/posts/page/${page + 1}`} />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  )
+}
