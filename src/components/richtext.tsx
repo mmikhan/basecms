3809ec4018154934c4e2410a20d8167b729568d6
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-08-27 19:10:31 +0200

    feat: remove unnecessary blocks from richtext

--- src/components/richtext.tsx
+++ src/components/richtext.tsx
@@ -10,31 +10,19 @@ import {
   RichText as ConvertRichText,
 } from '@payloadcms/richtext-lexical/react'
 import {
-  CallToActionBlock as CTABlockProps,
-  HighImpactHero as HighImpactHeroBlockProps,
-  MediumImpactHero as MediumImpactHeroBlockProps,
-  LowImpactHero as LowImpactHeroBlockProps,
-  ContentBlock as ContentBlockProps,
   MediaBlock as MediaBlockProps,
   CodeBlock as CodeBlockProps,
   BannerBlock as BannerBlockProps,
-  NavBlock as NavBlockProps,
-  FooterBlock as FooterBlockProps,
 } from '@/payload-types'
-import { CallToActionBlock } from '@/blocks/CallToAction/Component'
 import { cn } from '@/lib/utils'
-import { HighImpactHeroBlock } from '@/blocks/heros/HighImpact/Component'
-import { MediumImpactHeroBlock } from '@/blocks/heros/MediumImpact/Component'
-import { LowImpactHeroBlock } from '@/blocks/heros/LowImpact/Component'
-import { ContentBlock } from '@/blocks/Content/Component'
 import { MediaBlock } from '@/blocks/MediaBlock/Component'
 import { CodeBlock } from '@/blocks/Code/Component'
 import { BannerBlock } from '@/blocks/Banner/Component'
-import { NavBlock } from '@/blocks/Nav/Component'
-import { FooterBlockComponent } from '@/blocks/Footer/Component'
 
 // Extend the default node types with your custom blocks for full type safety
-type NodeTypes = DefaultNodeTypes | SerializedBlockNode<CTABlockProps>
+type NodeTypes =
+  | DefaultNodeTypes
+  | SerializedBlockNode<MediaBlockProps | CodeBlockProps | BannerBlockProps>
 
 const internalDocToHref = ({ linkNode }: { linkNode: SerializedLinkNode }) => {
   const { relationTo, value } = linkNode.fields.doc!
@@ -59,21 +47,6 @@ const jsxConverters: JSXConvertersFunction<NodeTypes> = ({ defaultConverters }) 
   ...defaultConverters,
   ...LinkJSXConverter({ internalDocToHref }),
   blocks: {
-    cta: ({ node }: { node: SerializedBlockNode<CTABlockProps> }) => (
-      <CallToActionBlock {...node.fields} />
-    ),
-    highImpactHero: ({ node }: { node: SerializedBlockNode<HighImpactHeroBlockProps> }) => (
-      <HighImpactHeroBlock {...node.fields} />
-    ),
-    mediumImpactHero: ({ node }: { node: SerializedBlockNode<MediumImpactHeroBlockProps> }) => (
-      <MediumImpactHeroBlock {...node.fields} />
-    ),
-    lowImpactHero: ({ node }: { node: SerializedBlockNode<LowImpactHeroBlockProps> }) => (
-      <LowImpactHeroBlock {...node.fields} />
-    ),
-    content: ({ node }: { node: SerializedBlockNode<ContentBlockProps> }) => (
-      <ContentBlock {...node.fields} />
-    ),
     mediaBlock: ({ node }: { node: SerializedBlockNode<MediaBlockProps> }) => (
       <MediaBlock {...node.fields} />
     ),
@@ -82,10 +55,6 @@ const jsxConverters: JSXConvertersFunction<NodeTypes> = ({ defaultConverters }) 
     ),
     banner: ({ node }: { node: SerializedBlockNode<BannerBlockProps> }) => (
       <BannerBlock {...node.fields} />
-    ),
-    nav: ({ node }: { node: SerializedBlockNode<NavBlockProps> }) => <NavBlock {...node.fields} />,
-    footerBlock: ({ node }: { node: SerializedBlockNode<FooterBlockProps> }) => (
-      <FooterBlockComponent {...node.fields} />
     ),
   },
 })
