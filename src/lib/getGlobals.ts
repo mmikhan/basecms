4d0a16c04f611c392cde91f9380211e2d1ba91ab
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-09-03 23:51:22 +0200

    feat: global cache func init

---
+++ src/lib/getGlobals.ts
@@ -0,0 +1,23 @@
+import type { Config } from '@/payload-types'
+import { unstable_cache } from 'next/cache'
+import { getPayload } from 'payload'
+import config from '@payload-config'
+
+/**
+ * Returns a unstable_cache function mapped with the cache tag for the slug
+ */
+export const getCachedGlobal = (slug: keyof Config['globals'], depth = 0) =>
+  unstable_cache(
+    async () => {
+      const payload = await getPayload({ config })
+
+      return await payload.findGlobal({
+        slug,
+        depth,
+      })
+    },
+    [slug],
+    {
+      tags: [`global_${slug}`],
+    },
+  )
