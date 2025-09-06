aee3556bd8322d0446ee1a7ccea1edffd2803fa6
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-09-06 15:35:40 +0200

    feat: refactor remote image url

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : undefined || process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typedRoutes: true,
@@ -26,16 +30,16 @@ const nextConfig = {
   images: {
     qualities: [25, 50, 75, 100],
     remotePatterns: [
-      ...(process.env.NODE_ENV !== 'production'
-        ? [
-            {
-              protocol: 'http',
-              hostname: 'localhost',
-              port: '3000',
-              pathname: '/api/media/file/**',
-            },
-          ]
-        : []),
+      ...[NEXT_PUBLIC_SERVER_URL].map((item) => {
+        const url = new URL(item)
+
+        return {
+          protocol: url.protocol.replace(':', ''),
+          hostname: url.hostname,
+          port: url.port,
+          pathname: '/**',
+        }
+      }),
     ],
   },
 }
