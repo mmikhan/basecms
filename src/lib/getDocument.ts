a0f564f06046430dd64c76393f3f953ee7842bc1
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-09-12 22:40:01 +0200

    feat: refactor get collection doc

--- src/lib/getDocument.ts
+++ src/lib/getDocument.ts
@@ -1,31 +1,34 @@
 import type { Config } from 'src/payload-types'
-
-import configPromise from '@payload-config'
-import { getPayload } from 'payload'
+import config from '@payload-config'
+import { CollectionSlug, getPayload } from 'payload'
 import { unstable_cache } from 'next/cache'
-
-type Collection = keyof Config['collections']
-
-async function getDocument(collection: Collection, slug: string, depth = 0) {
-  const payload = await getPayload({ config: configPromise })
-
-  const page = await payload.find({
-    collection,
-    depth,
-    where: {
-      slug: {
-        equals: slug,
-      },
-    },
-  })
-
-  return page.docs[0]
-}
 
 /**
  * Returns a unstable_cache function mapped with the cache tag for the slug
  */
-export const getCachedDocument = (collection: Collection, slug: string) =>
-  unstable_cache(async () => getDocument(collection, slug), [collection, slug], {
-    tags: [`${collection}_${slug}`],
-  })
+export const getCachedDocument = (
+  collection: keyof Config['collections'],
+  slug: CollectionSlug,
+  depth = 0,
+) =>
+  unstable_cache(
+    async () => {
+      const payload = await getPayload({ config })
+
+      const page = await payload.find({
+        collection,
+        depth,
+        where: {
+          slug: {
+            equals: slug,
+          },
+        },
+      })
+
+      return page.docs[0]
+    },
+    [collection, slug],
+    {
+      tags: [`${collection}_${slug}`],
+    },
+  )
