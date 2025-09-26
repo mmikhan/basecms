29aa20ad02a8846a07c4ac1f3ca589ea49152716
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-09-26 17:21:13 +0200

    feat: locale middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,33 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { isAuth } from './actions/auth'
+import createMiddleware from 'next-intl/middleware'
+import { routing } from './i18n/routing'
 
 export const config = {
   runtime: 'nodejs',
-  matcher: '/dashboard/:path*',
+  matcher: ['/((?!admin|api|trpc|next|_vercel|.*\\..*).*)'],
 }
 
+const handleI18nRouting = createMiddleware(routing)
+
 export async function middleware(request: NextRequest) {
-  const auth = await isAuth(request.headers)
+  if (request.nextUrl.pathname.includes('/dashboard')) {
+    const auth = await isAuth(request.headers)
 
-  if (!auth.user) {
-    const login = new URL('/login', request.url)
-    login.searchParams.set('redirect', request.nextUrl.pathname)
+    if (!auth.user) {
+      const pathSegments = request.nextUrl.pathname.split('/').filter(Boolean)
+      const potentialLocale = pathSegments[0]
+      const locale = routing.locales.includes(potentialLocale)
+        ? potentialLocale
+        : routing.defaultLocale
 
-    return NextResponse.redirect(login)
+      const loginUrl = new URL(`/${locale}/login`, request.url)
+      loginUrl.searchParams.set('redirect', request.nextUrl.pathname)
+
+      return NextResponse.redirect(loginUrl)
+    }
   }
 
-  return NextResponse.next()
+  return handleI18nRouting(request)
 }
