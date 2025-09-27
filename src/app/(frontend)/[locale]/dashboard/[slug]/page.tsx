d120eb24fe6040a56f035cb427fb8a3fec25beac
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-09-27 21:49:28 +0200

    feat: dashboard locale query

--- src/app/(frontend)/[locale]/dashboard/[slug]/page.tsx
+++ src/app/(frontend)/[locale]/dashboard/[slug]/page.tsx
@@ -1,28 +1,26 @@
 import { RenderBlocks } from '@/blocks/RenderBlocks'
 import { RefreshRouteOnSave } from '@/components/RefreshRouteOnSave'
-import { getCachedDocument } from '@/lib/getDocument'
-import type { Dashboard } from '@/payload-types'
 import { draftMode } from 'next/headers'
 import { notFound } from 'next/navigation'
-import { CollectionSlug, getPayload } from 'payload'
+import { type CollectionSlug, getPayload, type TypedLocale } from 'payload'
 import config from '@payload-config'
+import type { Locale } from 'next-intl'
+import { cache } from 'react'
+import { slugify } from 'transliteration'
 
 type DashboardSlugPageProps = {
-  params: Promise<{ slug?: string }>
+  params: Promise<{ slug?: string; locale: Locale }>
 }
 
 export default async function DashboardSlugPage({ params }: DashboardSlugPageProps) {
-  const { slug = 'dashboard' } = await params
+  const { slug = 'dashboard', locale } = await params
 
   const { isEnabled: draft } = await draftMode()
 
-  const page = (await getCachedDocument({
-    collection: 'dashboard',
+  const page = await queryPageBySlug({
     slug: slug as CollectionSlug,
-    draft,
-    limit: 1,
-    pagination: true,
-  })()) as Dashboard
+    locale: locale as TypedLocale,
+  })
 
   if (!page) return notFound()
 
@@ -35,6 +33,28 @@ export default async function DashboardSlugPage({ params }: DashboardSlugPagePro
     </>
   )
 }
+
+const queryPageBySlug = cache(
+  async ({ slug, locale }: { slug: CollectionSlug; locale: TypedLocale }) => {
+    const payload = await getPayload({ config })
+    const { isEnabled: draft } = await draftMode()
+
+    const result = await payload.find({
+      collection: 'dashboard',
+      locale,
+      draft,
+      limit: 1,
+      pagination: true,
+      where: {
+        slug: {
+          equals: slugify(decodeURIComponent(slug), { trim: true }),
+        },
+      },
+    })
+
+    return result.docs?.[0] || null
+  },
+)
 
 export async function generateStaticParams() {
   const payload = await getPayload({ config })
