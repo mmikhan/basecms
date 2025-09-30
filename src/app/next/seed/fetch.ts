195de1a81f940012938ffbffd73367f0cecb0a44
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-09-30 15:39:12 +0200

    feat: refactor fetch file type for seeder

--- src/app/next/seed/fetch.ts
+++ src/app/next/seed/fetch.ts
@@ -1,28 +1,22 @@
+import { fileTypeFromBuffer } from 'file-type'
+
 export const fetchFileByURL = async (url: string) => {
   const res = await fetch(url, {
     credentials: 'include',
     method: 'GET',
   })
 
-  if (!res.ok) {
-    throw new Error(`Failed to fetch file from ${url}, status: ${res.status}`)
-  }
+  if (!res.ok) throw new Error(`Failed to fetch file from ${url}, status: ${res.status}`)
 
   const data = await res.arrayBuffer()
+  const buffer = Buffer.from(data)
 
-  const extension = url.split('.').pop()?.toLowerCase()
-  let mimetype = `image/${extension}`
-
-  if (extension === 'jpg') {
-    mimetype = 'image/jpeg'
-  } else if (extension === 'svg') {
-    mimetype = 'image/svg+xml'
-  }
+  const fileType = await fileTypeFromBuffer(buffer)
 
   return {
     name: url.split('/').pop() ?? `file-${Date.now()}`,
     data: Buffer.from(data),
-    mimetype,
+    mimetype: fileType?.mime ?? 'application/octet-stream',
     size: data.byteLength,
   }
 }
