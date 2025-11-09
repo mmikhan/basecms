aa2591be17ec6b3f576e1e5ee4e1c10833cdbf29
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2025-11-09 20:28:02 +0100

    refactor: remove cookies from login block
    Prevents error on production from SSG built pages

--- src/blocks/Login/Component.tsx
+++ src/blocks/Login/Component.tsx
@@ -1,24 +1,13 @@
 import { LoginBlock as LoginBlockProps } from '@/payload-types'
 import { LoginForm } from './Form'
-import { cookies } from 'next/headers'
-import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
-import { SuccessIcon } from '@payloadcms/ui'
+import { Suspense } from 'react'
 
 export const LoginBlock: React.FC<LoginBlockProps> = async (props) => {
-  const verifiedEmail = (await cookies()).get('verified')?.value
-
   return (
-    <>
-      {verifiedEmail && (
-        <Alert variant="default" className="mb-4">
-          <SuccessIcon />
-          <AlertTitle>Your email has been verified</AlertTitle>
-          <AlertDescription>
-            Your email has been verified. Please login to your account.
-          </AlertDescription>
-        </Alert>
-      )}
-      <LoginForm {...props} />
-    </>
+    <div className="container max-w-sm mx-auto pt-32">
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm {...props} />
+      </Suspense>
+    </div>
   )
 }
