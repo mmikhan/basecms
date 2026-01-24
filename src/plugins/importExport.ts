1531bc8e464511644a762b112c7697bd369fd093
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2026-01-24 15:15:42 +0100

    chore: drop import static file dir

--- src/plugins/importExport.ts
+++ src/plugins/importExport.ts
@@ -1,8 +1,4 @@
 import { importExportPlugin } from '@payloadcms/plugin-import-export'
-import path, { dirname } from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
 
 export default importExportPlugin({
   overrideExportCollection: ({ collection }) => {
@@ -10,19 +6,11 @@ export default importExportPlugin({
       collection.admin.group = 'System'
     }
 
-    if (collection.upload && typeof collection.upload === 'object') {
-      collection.upload.staticDir = path.resolve(dirname(__filename), 'uploads')
-    }
-
     return collection
   },
   overrideImportCollection: ({ collection }) => {
     if (collection.admin) {
       collection.admin.group = 'System'
-    }
-
-    if (collection.upload && typeof collection.upload === 'object') {
-      collection.upload.staticDir = path.resolve(dirname(__filename), 'uploads')
     }
 
     return collection
